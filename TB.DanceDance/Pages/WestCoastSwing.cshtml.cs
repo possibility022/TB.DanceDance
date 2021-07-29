d398ad15b28fb6e9fb0fbbc7df5ddf51a82bae98
Possibility <tomaszbakk@outlook.com>
2021-07-29 16:16:18 +0200

    I was working on ASP

--- TB.DanceDance/Pages/WestCoastSwing.cshtml.cs
+++ TB.DanceDance/Pages/WestCoastSwing.cshtml.cs
@@ -1,28 +1,19 @@
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using TB.DanceDance.Data;
-using TB.DanceDance.Data.Models;
 
 namespace TB.DanceDance.Pages
 {
     [Authorize]
     public class WestCoastSwingModel : PageModel
     {
-        private readonly ApplicationDbContext context;
-
         public void OnGet()
         {
-            WestCoastSwingVideos = context.VideosInformation.ToList();
         }
 
         public WestCoastSwingModel(ApplicationDbContext context)
         {
-            this.context = context;
-        }
 
-        public IList<VideoInformation> WestCoastSwingVideos { get; set; }
+        }
     }
 }
