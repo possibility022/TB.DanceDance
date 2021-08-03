a047c0ad2798ea88945701e9ac79262e804d470a
Possibility <tomaszbakk@outlook.com>
2021-08-04 00:45:33 +0200

    Basic controller

---
+++ TB.DanceDance.API/Controllers/VideosController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using TB.DanceDance.Data.Db;
+using TB.DanceDance.Data.Models;
+
+namespace TB.DanceDance.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VideosController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly ILogger<VideosController> _logger;
+
+        public VideosController(ApplicationDbContext context, ILogger<VideosController> logger)
+        {
+            this.context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<VideoInformation> Get()
+        {
+            return context.VideosInformation.AsEnumerable();
+        }
+    }
+}
---
+++ TB.DanceDance.Data.Db/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace TB.DanceDance.Data.Db
+{
+    public static class Constants
+    {
+        public static class Infrastructure
+        {
+            public const string VideoBlobContainerName = "videos";
+        }
+    }
+}
