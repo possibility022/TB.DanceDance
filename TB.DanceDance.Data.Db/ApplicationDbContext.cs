8c03c3809acc488fc817344de3d637ef2a03b9ce
Possibility <tomaszbakk@outlook.com>
2021-08-04 00:26:37 +0200

    Moving context to different project

---
+++ TB.DanceDance.Data.Db/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TB.DanceDance.Data.Models;
+
+namespace TB.DanceDance.Data.Db
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        // Quick workaround
+        public static string ConnectionString = null;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            if (ConnectionString != null)
+                optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public ApplicationDbContext()
+        {
+            
+        }
+
+        public DbSet<VideoInformation> VideosInformation { get; set; }
+    }
+}
---
+++ TB.DanceDance.Data.Db/ApplicationDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TB.DanceDance.Data.Db
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer("Data Source=blog.db"); //todo change this.
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
