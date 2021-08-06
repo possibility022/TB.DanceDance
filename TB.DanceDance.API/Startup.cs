c9deda314f180f87ff8a0aa51ad0e682c4077088
Possibility <tomaszbakk@outlook.com>
2021-08-06 09:59:24 +0200

    Deployed. It's working

--- TB.DanceDance.API/Startup.cs
+++ TB.DanceDance.API/Startup.cs
@@ -29,6 +29,8 @@ namespace TB.DanceDance.API
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"]);
+
             services.AddCors(options =>
             {
                 options.AddDefaultPolicy(builder =>
@@ -52,6 +54,7 @@ namespace TB.DanceDance.API
             services.AddScoped<IBlobDataService, BlobDataService>(provider => new BlobDataService(Configuration.GetConnectionString("BlobServiceConnectionString")));
 
             services.AddControllers();
+            services.AddLogging();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
