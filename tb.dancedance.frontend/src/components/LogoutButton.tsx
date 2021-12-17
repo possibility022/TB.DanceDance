1e26083303aaa2b6143fb3bab83a678079e65107
Possibility <tomaszbakk@outlook.com>
2021-12-17 04:34:45 +0100

    Working on style

--- tb.dancedance.frontend/src/components/LogoutButton.tsx
+++ tb.dancedance.frontend/src/components/LogoutButton.tsx
@@ -1,13 +1,15 @@
 import React from "react"
 import { useAuth0 } from "@auth0/auth0-react"
+import "bulma/css/bulma.min.css"
+import { Button } from "react-bulma-components"
 
 const LogoutButton = () => {
 	const { logout } = useAuth0()
 
 	return (
-		<button onClick={() => logout({ returnTo: window.location.origin })}>
+		<Button onClick={() => logout({ returnTo: window.location.origin })}>
       Log Out
-		</button>
+		</Button>
 	)
 }
 
