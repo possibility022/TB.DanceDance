daaedd10386731c496e1e670bee9e42eced986fe
Possibility <tomaszbakk@outlook.com>
2021-12-17 03:53:16 +0100

    Deleting old web app

--- tb.dancedance.web/src/store/configureStore.ts
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import RootReducer from "../reduces/RootReducer"
-
-const Store = configureStore({ reducer: RootReducer})
-
-export type RootState = ReturnType<typeof RootReducer>
-export default Store
--- tb.dancedance.web/src/types/videoinformation.d.ts
+++
@@ -1,8 +0,0 @@
-export default interface VideoInformation {
-    id: number
-    name: string
-    blobId: string
-    type: number
-    creationTimeUtc: datetime
-    duration: timestamp
-}
