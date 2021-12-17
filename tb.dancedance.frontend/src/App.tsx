00d227bf847730bf97f0fee9fd5579b859dd538c
Possibility <tomaszbakk@outlook.com>
2021-12-17 04:22:21 +0100

    It is working :O

--- tb.dancedance.frontend/src/App.tsx
+++ tb.dancedance.frontend/src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react"
 import logo from "./logo.svg"
+import LoginButton from "./components/LoginButton"
+import LogoutButton from "./components/LogoutButton"
 import "./App.css"
+import { useAuth0 } from "@auth0/auth0-react"
 
 function App() {
+
+	const { user, isAuthenticated, isLoading } = useAuth0()
+
 	return (
 		<section className="section">
 			<div className="container">
@@ -12,6 +18,19 @@ function App() {
 				<p className="subtitle">
 					My first website with <strong>Bulma</strong>!
 				</p>
+
+				<LoginButton></LoginButton>
+				<LogoutButton></LogoutButton>
+
+				{
+					isAuthenticated && (
+						<div>
+							<img src={user?.picture} alt={user?.name} />
+							<h2>{user?.name}</h2>
+							<p>{user?.email}</p>
+						</div>
+
+					)}
 			</div>
 		</section>
 	)
