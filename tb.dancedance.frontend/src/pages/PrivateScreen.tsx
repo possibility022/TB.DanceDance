8a540bbfac60e7348e8dc7882cd64ee0bd7550e6
Possibility <tomaszbakk@outlook.com>
2022-10-13 21:11:40 +0200

    This was kind of working

--- tb.dancedance.frontend/src/pages/PrivateScreen.tsx
+++ tb.dancedance.frontend/src/pages/PrivateScreen.tsx
@@ -1,10 +1,74 @@
+import { Blob } from 'buffer';
 import * as React from 'react';
+import { useEffect, useState } from 'react';
+import ReactPlayer from 'react-player';
+import { AuthConsumer } from '../providers/AuthProvider';
+import { AuthService, IAuthService, TokenProvider } from '../services/AuthService';
+import { VideoInfoService } from '../services/VideoInfoService';
+
+interface IVideoPlayProps {
+  videoUrl: string
+}
+
+export function PrivateScreen(props: IVideoPlayProps) {
 
 
-export function PrivateScreen () {
-  return (
-    <div>
-      Czy to działa?
-    </div>
-  );
+  const fetchVideo = async () => {
+    const response = await fetch(props.videoUrl, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    const blob = await response.blob()
+    const url = URL.createObjectURL(blob as Blob)
+    setUrl(url)
+    //const url = URL.createObjectURL(blob)
+    //setUrl(url)
+  }
+
+  const [url, setUrl] = useState<string>()
+  const [token, setToken] = useState<string>('')
+
+  const tokenProvider: TokenProvider = new AuthService()
+
+  useEffect(() => {
+    const t = tokenProvider.getAccessToken()
+    if (t != token) {
+      setToken(token)
+      fetchVideo()
+        .catch(e => console.error(e))
+    }
+  },[url])
+
+  return <AuthConsumer>
+    {
+      ({ getAccessToken }: IAuthService) => {
+        return (
+          <ReactPlayer
+            url={url}
+            controls={true}
+          // config={{
+          //   file: {
+          //     forceHLS: true,
+          //     hlsOptions: {
+          //       xhrSetup: function (xhr: XMLHttpRequest, url: unknown) {
+          //         console.log(xhr, url)
+          //         const token = getAccessToken()
+          //         if (token)
+          //           xhr.setRequestHeader('Authorization', `Bearer ${token}`)
+          //         else
+          //           console.error("token not provided")
+          //       }
+          //     }
+          //   }
+          // }
+          // }
+          >
+
+          </ReactPlayer>
+
+        )
+      }
+    }
+  </AuthConsumer >
 }
