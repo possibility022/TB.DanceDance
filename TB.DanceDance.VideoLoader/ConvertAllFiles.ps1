a6ff9d693e5a8c82806c63186b951bf8c0aa9498
Possibility <tomaszbakk@outlook.com>
2022-10-15 04:26:25 +0200

    Converting files script

---
+++ TB.DanceDance.VideoLoader/ConvertAllFiles.ps1
@@ -0,0 +1,22 @@
+$ffmpg = "C:\Users\TomaszBak\Downloads\ffmpeg-master-latest-win64-gpl\bin\ffmpeg.exe";
+$commandArgs = '-i [INPUT_FILE_PATH] -c:v libvpx-vp9 -b:v 2M [OUTPUT_FILE_PATH]'
+$folderWithVideos = "G:\West"
+
+$skipVideos = @(
+    "20220209_183940"
+)
+
+$filesToConvert = Get-ChildItem -Path $folderWithVideos -Filter "*.mp4"
+$filesToConvert = $filesToConvert | Where-Object $skipVideos.Contains($_.Name) -eq $fase
+
+foreach($file in $filesToConvert)
+{
+    Write-Output "Converting ${$file.FullName}"
+    $outputFile = $file.FullName.TrimEnd($file.Extension) + ".webm"
+    $a = $commandArgs.Replace("[INPUT_FILE_PATH]", $file.FullName).Replace("[OUTPUT_FILE_PATH]", $outputFile)
+
+    $command = "$ffmpg $a"
+    Write-Output "Invoking command: '$command'"
+    Invoke-Expression -Command $command
+}
+
