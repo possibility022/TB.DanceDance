6853eba7df9cd77f78cae544db41579ef8a3e1f8
Possibility <tomaszbakk@outlook.com>
2022-11-05 18:47:24 +0100

    Do not throw

--- TB.DanceDance.API/Controllers/VideoController.cs
+++ TB.DanceDance.API/Controllers/VideoController.cs
@@ -34,8 +34,7 @@ public class VideoController : Controller
     public async Task<IActionResult> GetStreamAsync(string guid, [FromQuery] string token)
     {
         // todo create better authentication. Send send tokens in headers
-
-        var validationRes = await tokenValidator.ValidateAccessTokenAsync(token);
+        var validationRes = await tokenValidator.ValidateAccessTokenAsync(token ?? string.Empty);
         if (validationRes == null)
             // Idk when this can happen
             throw new Exception("Results of validation are null.");
