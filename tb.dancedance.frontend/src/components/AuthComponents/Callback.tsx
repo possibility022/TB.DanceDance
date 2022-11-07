1d9611054422d3f0c709fb5e28911709c1a9d8da
Possibility <tomaszbakk@outlook.com>
2022-11-07 19:38:16 +0100

    Navigate to home

--- tb.dancedance.frontend/src/components/AuthComponents/Callback.tsx
+++ tb.dancedance.frontend/src/components/AuthComponents/Callback.tsx
@@ -1,11 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import { AuthConsumer } from "../../providers/AuthProvider";
 import { IAuthService } from "../../services/AuthService";
 
 export const Callback = () => {
+
+    const navigate = useNavigate()
+
     return <AuthConsumer>
         {({ signinRedirectCallback }: IAuthService) => {
             signinRedirectCallback()
                 .then(() => {
+                    navigate('/')
                     // todo: check why redirect urls stays in URL address
                 })
                 .catch(e => console.error(e));
