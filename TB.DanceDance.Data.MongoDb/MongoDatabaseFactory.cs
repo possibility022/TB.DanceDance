90f6c0692960ebf56dc793d226059a0027dc0394
Possibility <tomaszbakk@outlook.com>
2023-01-24 02:06:22 +0100

    Revert "Working on"
    This reverts commit bbfc6c50a99ce8755f86aac61e9cba1483fbdb5c.

---
+++ TB.DanceDance.Data.MongoDb/MongoDatabaseFactory.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+
+namespace TB.DanceDance.Data.MongoDb
+{
+    public class MongoDatabaseFactory
+    {
+        public static MongoClient GetClient()
+        {
+            var connectionString = Environment.GetEnvironmentVariable("TB.DanceDance.ConnectionString.Mongo");
+            if (string.IsNullOrEmpty(connectionString))
+                throw new Exception("Connection string not available in environment variables");
+
+            var client = new MongoClient(connectionString);
+            return client;
+        }
+    }
+}
