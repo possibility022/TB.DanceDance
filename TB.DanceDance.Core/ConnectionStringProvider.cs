378ad22619ae9908368f261379723d40caeb5cfd
Possibility <tomaszbakk@outlook.com>
2023-02-19 01:45:35 +0100

    Moving secrets to dotnet user secrets

---
+++ TB.DanceDance.Core/ConnectionStringProvider.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace TB.DanceDance.Core
+{
+    public static class ConnectionStringProvider
+    {
+        public static string GetMongoDbConnectionString(IConfiguration configuration)
+        {
+            var section = configuration.GetRequiredSection("ConnectionStrings:MongoDB");
+
+            var cs = section.Value;
+
+            if (cs == null)
+                cs = Environment.GetEnvironmentVariable("TB.DanceDance.ConnectionString.Mongo");
+
+
+            if (string.IsNullOrEmpty(cs))
+                throw new Exception("Could not resolve connection string for mongo db.");
+
+            return cs;
+        }
+
+        public static string GetBlobConnectionString(IConfiguration configuration)
+        {
+            // I know I know, it is a copy-paste...
+
+            var section = configuration.GetRequiredSection("ConnectionStrings:Blob");
+
+            var cs = section.Value;
+
+            if (cs == null)
+                cs = Environment.GetEnvironmentVariable("TB.DanceDance.ConnectionString.Blob");
+
+
+            if (string.IsNullOrEmpty(cs))
+                throw new Exception("Could not resolve connection string for mongo db.");
+
+            return cs;
+        }
+    }
+}
