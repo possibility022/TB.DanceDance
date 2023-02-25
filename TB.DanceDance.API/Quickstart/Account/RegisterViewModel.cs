8165545784a145cea6da2ffbde53bff7917d3707
Possibility <tomaszbakk@outlook.com>
2023-02-25 15:51:56 +0100

    Extend registration

--- TB.DanceDance.API/Quickstart/Account/RegisterViewModel.cs
+++ TB.DanceDance.API/Quickstart/Account/RegisterViewModel.cs
@@ -6,10 +6,19 @@ namespace TB.DanceDance.API.Quickstart.Account
     {
         [Required]
         public string Email { get; set; } = null!;
+        
+        [Required]
+        public string FirstName { get; set; } = null!;
+        
+        [Required]
+        public string LastName { get; set; } = null!;
+        
         [Required]
         public string Password { get; set; } = null!;
+        
         [Required]
         public string ConfirmPassword { get; set; } = null!;
+        
         public string? ReturnUrl { get; set; }
     }
 }
