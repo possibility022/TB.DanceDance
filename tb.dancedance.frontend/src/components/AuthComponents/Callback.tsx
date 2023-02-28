7b80d88cd8403e0242d60748a468645211001f00
Possibility <tomaszbakk@outlook.com>
2023-02-28 23:53:00 +0100

    Trying to solve issue with refresh-token

--- tb.dancedance.frontend/src/components/AuthComponents/Callback.tsx
+++ tb.dancedance.frontend/src/components/AuthComponents/Callback.tsx
@@ -1,21 +1,35 @@
+import { useCallback, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { AuthConsumer } from "../../providers/AuthProvider";
-import { IAuthService } from "../../services/AuthService";
+import { AuthContext } from "../../providers/AuthProvider";
 
 export const Callback = () => {
 
     const navigate = useNavigate()
 
-    return <AuthConsumer>
-        {({ signinRedirectCallback }: IAuthService) => {
-            signinRedirectCallback()
-                .then(() => {
-                    navigate('/')
-                    // todo: check why redirect urls stays in URL address
-                })
-                .catch(e => console.error(e));
-            return <span>:D</span>;
-        }}
-    </AuthConsumer>
+    const authContext = useContext(AuthContext)
+
+    const callback = useCallback(() => {
+        authContext.signinRedirectCallback()
+            .then(() => {
+                navigate('/')
+            })
+            .catch((e) => {
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+                if (e['error'] === 'login_required') {
+                    authContext.signinRedirect()
+                        .catch(console.error)
+                }
+                console.log(e)
+                navigate('/') // todo navigate to returned url
+            },)
+
+        return ":)"
+    }, [])
+
+    // dont look at that :| I don't know how to do it in proper way at this moment
+    return (<div>
+        {callback()}
+    </div>)
+
 
 }
