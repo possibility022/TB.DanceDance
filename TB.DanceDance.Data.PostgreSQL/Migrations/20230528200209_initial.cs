0b868d1b1a7733f07f8e5354b685af4a39d17eff
Possibility <tomaszbakk@outlook.com>
2023-05-28 22:09:14 +0200

    Initial db

---
+++ TB.DanceDance.Data.PostgreSQL/Migrations/20230528200209_initial.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TB.DanceDance.Data.PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Date = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Videos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    BlobId = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    UploadedBy = table.Column<string>(type: "text", nullable: false),
+                    RecordedDateTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    SharedDateTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    Duration = table.Column<TimeOnly>(type: "time without time zone", nullable: false),
+                    MetadataAsJson = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Videos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EventAssigmentRequests",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    EventId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventAssigmentRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EventAssigmentRequests_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GroupAssigmentRequests",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    GroupId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GroupAssigmentRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GroupAssigmentRequests_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SharedWith",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    VideoId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    EventId = table.Column<Guid>(type: "uuid", nullable: true),
+                    GroupId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SharedWith", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SharedWith_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_SharedWith_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_SharedWith_Videos_VideoId",
+                        column: x => x.VideoId,
+                        principalTable: "Videos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventAssigmentRequests_EventId",
+                table: "EventAssigmentRequests",
+                column: "EventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupAssigmentRequests_GroupId",
+                table: "GroupAssigmentRequests",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SharedWith_EventId",
+                table: "SharedWith",
+                column: "EventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SharedWith_GroupId",
+                table: "SharedWith",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SharedWith_VideoId",
+                table: "SharedWith",
+                column: "VideoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventAssigmentRequests");
+
+            migrationBuilder.DropTable(
+                name: "GroupAssigmentRequests");
+
+            migrationBuilder.DropTable(
+                name: "SharedWith");
+
+            migrationBuilder.DropTable(
+                name: "Events");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+
+            migrationBuilder.DropTable(
+                name: "Videos");
+        }
+    }
+}
