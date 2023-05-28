72be7d496e5db44033fc1c0f40a17c86d9a70e5d
Possibility <tomaszbakk@outlook.com>
2023-05-28 22:09:14 +0200

    Initial db

---
+++ TB.DanceDance.Data.PostgreSQL/DanceDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TB.DanceDance.Data.PostgreSQL.Models;
+
+namespace TB.DanceDance.Data.PostgreSQL
+{
+    public class DanceDbContext : DbContext
+    {
+        public DanceDbContext(DbContextOptions<DanceDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+
+        DbSet<Video> Videos { get; set; }
+        DbSet<GroupAssigmentRequest> GroupAssigmentRequests { get; set; }
+        DbSet<EventAssigmentRequest> EventAssigmentRequests { get; set; }
+        DbSet<SharedWith> SharedWith { get; set; }
+        DbSet<Group> Groups { get; set; }
+        DbSet<Event> Events { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EventAssigmentRequest>()
+                .HasOne<Event>()
+                .WithMany()
+                .HasForeignKey(e => e.EventId)
+                .IsRequired();
+
+            modelBuilder.Entity<GroupAssigmentRequest>()
+                .HasOne<Group>()
+                .WithMany()
+                .HasForeignKey(e => e.GroupId)
+                .IsRequired();
+        }
+
+    }
+}
---
+++ TB.DanceDance.Data.PostgreSQL/Models/Video.cs
@@ -0,0 +1,71 @@
+﻿namespace TB.DanceDance.Data.PostgreSQL.Models;
+
+public class Video
+{
+    public Guid Id { get; set; }
+    public required string BlobId { get; init; }
+    public string Name { get; set; }
+
+    // User Id
+    public required string UploadedBy { get; init; }
+    public required DateTimeOffset RecordedDateTime { get; init; }
+    public required DateTimeOffset SharedDateTime { get; init; }
+    public required TimeOnly Duration { get; init; }
+    public Guid MetadataAsJson { get; init; }
+
+    // Navigation properties
+    public ICollection<SharedWith> SharedWith { get; set; } = null!;
+}
+
+public class GroupAssigmentRequest
+{
+    public Guid Id { get; set; }
+    public required string UserId { get; init; }
+    public required Guid GroupId { get; init; }
+}
+
+public class EventAssigmentRequest
+{
+    public Guid Id { get; set; }
+    public required string UserId { get; init; }
+    public required Guid EventId { get; init; }
+}
+
+public class SharedWith
+{
+    public Guid Id { get; set; }
+    public required Guid VideoId { get; init; }
+    public required string UserId { get; init; }
+    public Guid? EventId { get; set; }
+    public Guid? GroupId { get; set; }
+
+    public Video Video { get; set; } = null!;
+    public Event? Event { get; set; }
+    public Group? Group { get; set; }
+}
+
+public class Group
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+
+    public ICollection<SharedWith> HasSharedVideos { get; set; } = null!;
+}
+
+public class Event
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required DateTimeOffset Date { get; init; }
+    public required EventType Type { get; init; }
+
+    public ICollection<SharedWith> HasSharedVideos { get; set; } = null!;
+}
+
+public enum EventType
+{
+    Unknown = 0,
+    PointedEvent,
+    MediumNotPointed,
+    SmallWorkshop
+}
