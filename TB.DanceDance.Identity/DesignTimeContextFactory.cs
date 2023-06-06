7640ede923a4a390836935576679a9e45f226f36
Possibility <tomaszbakk@outlook.com>
2023-06-07 01:12:04 +0200

    Identity seems to work

---
+++ TB.DanceDance.Identity/DesignTimeContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace TB.DanceDance.Identity
+{
+    public class DesignTimeContextFactory : IDesignTimeDbContextFactory<IdentityStoreContext>
+    {
+        public IdentityStoreContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<IdentityStoreContext>();
+            optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Userid=postgres;Password=rgFraWIuyxONqWCQ71wh;Database=identitystore");
+
+            return new IdentityStoreContext(optionsBuilder.Options);
+        }
+    }
+}
