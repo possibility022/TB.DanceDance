f0d9166cfd550ceebcc6bb0a3de4c5f4e740cb05
Possibility <tomaszbakk@outlook.com>
2023-06-10 16:14:35 +0200

    db update - Videos to transform

---
+++ TB.DanceDance.Data.PostgreSQL/Migrations/20230610141412_AddingVideosToTransform.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TB.DanceDance.Data.PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingVideosToTransform : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VideosToTranform",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    BlobId = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    UploadedBy = table.Column<string>(type: "text", nullable: false),
+                    RecordedDateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    SharedDateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Duration = table.Column<TimeSpan>(type: "interval", nullable: true),
+                    SharedWithId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AssignedToEvent = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VideosToTranform", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VideosToTranform");
+        }
+    }
+}
