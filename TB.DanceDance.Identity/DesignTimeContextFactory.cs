7dee3b6fbe5fe8d39c84634ca5cb37cfd988c205
Possibility <tomaszbakk@outlook.com>
2023-06-17 19:45:00 +0200

    Migrating IdentityServer storage to PostgressDb. Changing used package to cnblogs.IdentityServer. It supports .net 7.  https://github.com/IdentityServer/IdentityServer4/issues/5486#issuecomment-1321024688

--- TB.DanceDance.Identity/DesignTimeContextFactory.cs
+++ TB.DanceDance.Identity/DesignTimeContextFactory.cs
@@ -1,16 +1,58 @@
 ﻿using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore;
+using IdentityServer4.EntityFramework.DbContexts;
 
 namespace TB.DanceDance.Identity
 {
-    public class DesignTimeContextFactory : IDesignTimeDbContextFactory<IdentityStoreContext>
+    public class DesignTimeContextFactory : 
+        IDesignTimeDbContextFactory<IdentityStoreContext>,
+        IDesignTimeDbContextFactory<PersistedGrantDbContext>,
+        IDesignTimeDbContextFactory<ConfigurationDbContext>
+
     {
-        public IdentityStoreContext CreateDbContext(string[] args)
+
+        public static string GetMigrationAssembly()
+        {
+#pragma warning disable CS8603 // Possible null reference return.
+            return typeof(DesignTimeContextFactory).Assembly.GetName().Name;
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+
+        IdentityStoreContext IDesignTimeDbContextFactory<IdentityStoreContext>.CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<IdentityStoreContext>();
             optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Userid=postgres;Password=rgFraWIuyxONqWCQ71wh;Database=identitystore");
 
             return new IdentityStoreContext(optionsBuilder.Options);
+        }
+
+        ConfigurationDbContext IDesignTimeDbContextFactory<ConfigurationDbContext>.CreateDbContext(string[] args)
+        {
+            var assemblyName = GetMigrationAssembly();
+
+            var optionsBuilder = new DbContextOptionsBuilder<ConfigurationDbContext>();
+            optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Userid=postgres;Password=rgFraWIuyxONqWCQ71wh;Database=identitystore",
+                b => b.MigrationsAssembly(assemblyName)
+                ); ;
+
+            return new ConfigurationDbContext(optionsBuilder.Options, new IdentityServer4.EntityFramework.Options.ConfigurationStoreOptions()
+            {
+
+            });
+        }
+
+        PersistedGrantDbContext IDesignTimeDbContextFactory<PersistedGrantDbContext>.CreateDbContext(string[] args)
+        {
+            var assemblyName = GetMigrationAssembly();
+
+            var optionsBuilder = new DbContextOptionsBuilder<PersistedGrantDbContext>();
+            optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Userid=postgres;Password=rgFraWIuyxONqWCQ71wh;Database=identitystore",
+                b => b.MigrationsAssembly(assemblyName));
+
+            return new PersistedGrantDbContext(optionsBuilder.Options, new IdentityServer4.EntityFramework.Options.OperationalStoreOptions
+            {
+
+            });
         }
     }
 }
