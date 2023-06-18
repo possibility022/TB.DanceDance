10e8c0629035ad71bb5b878bef2736dd5c324716
Possibility <tomaszbakk@outlook.com>
2023-06-18 18:49:39 +0200

    Api endpoints updates

--- TB.DanceDance.API/ApiEndpoints.cs
+++ TB.DanceDance.API/ApiEndpoints.cs
@@ -8,20 +8,20 @@ public static class ApiEndpoints
     {
         private const string Base = $"{ApiBase}/videos"; //todo to plural
 
-        public const string GetAll = $"{Base}/getinformation";
-        public const string GetSingle = $"{Base}/{{guid}}/getinformation";
-        public const string GetStream = $"{Base}/stream/{{guid}}";
+        public const string GetAll = $"{Base}";
+        public const string GetSingle = $"{Base}/{{guid}}";
+        public const string GetStream = $"{Base}/{{guid}}/stream";
 
         public const string Rename = $"{Base}/{{guid}}/rename";
-        public const string GetUploadUrl = $"{Base}/getUploadUrl";
+        public const string GetUploadUrl = $"{Base}/upload";
 
 
         public static class Access
         {
-            private const string Base = $"{Video.Base}/access";
+            private const string Base = $"{Video.Base}/accesses";
 
-            public const string GetAll = $"{Base}/getall";
-            public const string GetUserAccess = $"{Base}/user";
+            public const string GetAll = $"{Base}";
+            public const string GetUserAccess = $"{Base}/my";
             public const string RequestAccess = $"{Base}/request";
         }
 
