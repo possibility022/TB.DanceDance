6e4d48d592d6a45b43819bef042432d92a8dd097
Possibility <tomaszbakk@outlook.com>
2023-07-08 00:25:29 +0200

    Adding migration

---
+++ TB.DanceDance.Data.PostgreSQL/Migrations/20230707222445_NewFieldsForVideoUpload.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TB.DanceDance.Data.PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewFieldsForVideoUpload : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LockedTill",
+                schema: "video",
+                table: "ToTransform",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Metadata",
+                schema: "video",
+                table: "ToTransform",
+                type: "bytea",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LockedTill",
+                schema: "video",
+                table: "ToTransform");
+
+            migrationBuilder.DropColumn(
+                name: "Metadata",
+                schema: "video",
+                table: "ToTransform");
+        }
+    }
+}
