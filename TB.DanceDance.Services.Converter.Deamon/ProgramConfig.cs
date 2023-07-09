4af037a134c3a159a39c265954dd9fe5e4987bba
Possibility <tomaszbakk@outlook.com>
2023-07-09 21:51:28 +0200

    More configurations

--- TB.DanceDance.Services.Converter.Deamon/ProgramConfig.cs
+++ TB.DanceDance.Services.Converter.Deamon/ProgramConfig.cs
@@ -10,6 +10,9 @@ internal class ProgramConfig
         public const string FFMPGPath = "D:\\Programy\\ffmpeg-2022-12-04-git-6c814093d8-full_build\\bin\\ffmpeg.exe";
         public const string FFProbePath = "D:\\Programy\\ffmpeg-2022-12-04-git-6c814093d8-full_build\\bin\\ffprobe.exe";
         public const string FFMPGFolder = "D:\\Programy\\ffmpeg-2022-12-04-git-6c814093d8-full_build\\bin";
+
+        public static string ApiOrigin { get; internal set; } = string.Empty;
+        public static string OAuthOrigin { get; internal set; } = string.Empty;
     }
 
     public static TokenProviderOptions TokenProviderOptions { get; private set; } = null!;
@@ -19,18 +22,27 @@ internal class ProgramConfig
     {
         ConfigureLogging();
         ConfigureFfmpeg();
+        ConfigureApi();
         ConfigureAuth();
     }
 
     private static void ConfigureAuth()
     {
-        var lines = File.ReadAllLines("auth.settings");
+        var lines = File.ReadAllLines("auth.settings.txt");
         TokenProviderOptions = new TokenProviderOptions()
         {
             ClientId = lines[0].Trim(),
             ClientSecret = lines[1].Trim(),
             Scope = lines[2].Trim()
         };
+
+        Settings.OAuthOrigin = lines[3];
+    }
+
+    private static void ConfigureApi()
+    {
+        var lines = File.ReadAllLines("api.settings.txt");
+        Settings.ApiOrigin = lines[0].Trim();
     }
 
     private static void ConfigureFfmpeg()
