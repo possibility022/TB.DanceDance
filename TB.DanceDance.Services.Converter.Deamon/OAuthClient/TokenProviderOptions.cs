b5739be672500f867b8b535e7479895aae0789cb
Possibility <tomaszbakk@outlook.com>
2023-07-09 19:00:35 +0200

    Working converter - todo : upload

---
+++ TB.DanceDance.Services.Converter.Deamon/OAuthClient/TokenProviderOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace TB.DanceDance.Services.Converter.Deamon.OAuthClient;
+
+class TokenProviderOptions
+{
+    public string Scope { get; init; }
+    public string ClientId { get; init; }
+    public string ClientSecret { get; init; }
+}
---
+++ TB.DanceDance.Services.Converter.Deamon/OAuthClient/TokenResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TB.DanceDance.Services.Converter.Deamon;
+
+record TokenResponse
+{
+    [JsonPropertyName("access_token")]
+    public string AccessToken { get; set; } = null!;
+
+    [JsonPropertyName("expires_in")]
+    public int ExpiresIn { get; set; }
+
+    [JsonPropertyName("token_type")]
+    public string TokenType { get; set; } = null!;
+}
