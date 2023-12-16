9ee705abcd311034cf5863a5ff5ebeadd8c1105a
Possibility <tomaszbakk@outlook.com>
2023-12-16 18:28:55 +0100

    Redefine migration

---
+++ TB.DanceDance.Data.PostgreSQL/Migrations/20231216172502_MergeVideosToConvertIntoVideos.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TB.DanceDance.Data.PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class MergeVideosToConvertIntoVideos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Metadata",
+                schema: "video",
+                table: "Videos");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BlobId",
+                schema: "video",
+                table: "Videos",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "BlobId",
+                schema: "video",
+                table: "Videos",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Metadata",
+                schema: "video",
+                table: "Videos",
+                type: "bytea",
+                nullable: true);
+        }
+    }
+}
