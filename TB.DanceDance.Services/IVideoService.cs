2a5e36f2b9d83796f0a84be206a66e9e48fc1a41
Possibility <tomaszbakk@outlook.com>
2023-12-16 18:31:12 +0100

    remove not used methods

--- TB.DanceDance.Services/IVideoService.cs
+++ TB.DanceDance.Services/IVideoService.cs
@@ -1,5 +1,4 @@
 ﻿using TB.DanceDance.Data.Blobs;
-using TB.DanceDance.Data.PostgreSQL.Models;
 using TB.DanceDance.Services.Models;
 
 namespace TB.DanceDance.Services;
@@ -9,14 +8,7 @@ public interface IVideoService
     Task<Stream> OpenStream(string blobName);
     IQueryable<VideoInfo> GetVideos(string userId);
     Task<VideoInfo?> GetVideoByBlobAsync(string userId, string blobId);
-    Task<IQueryable<Video>> GetVideos();
-    Task<Video> UploadVideoAsync(string filePath, CancellationToken cancellationToken);
-
-    Task<Event> GetEvent(Guid id);
-    Task<Group> GetGroup(Guid id);
-
     Task<bool> DoesUserHasAccessAsync(string videoBlobId, string userId);
     Task<bool> RenameVideoAsync(Guid guid, string newName);
-
     Task<SharedBlob> GetSharingLink(string userId, string name, string fileName, bool assignedToEvent, Guid sharedWith);
 }
