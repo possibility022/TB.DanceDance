a48a37368987fc4867b161ca60fe2a67ddda27ba
Possibility <tomaszbakk@outlook.com>
2024-02-25 02:26:52 +0100

    Storing user name and last name

---
+++ TB.DanceDance.Identity/Data/Migrations/Identity/IdentityStore/20240225012625_AddUserFullName.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TB.DanceDance.Identity.Data.Migrations.Identity.IdentityStore
+{
+    /// <inheritdoc />
+    public partial class AddUserFullName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                schema: "Idp.Ident",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                schema: "Idp.Ident",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                schema: "Idp.Ident",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                schema: "Idp.Ident",
+                table: "AspNetUsers");
+        }
+    }
+}
