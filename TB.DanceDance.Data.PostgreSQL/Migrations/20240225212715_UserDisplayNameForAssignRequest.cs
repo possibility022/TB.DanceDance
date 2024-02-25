e6a445432962ceb1cb00920222281f4d569c1464
Possibility <tomaszbakk@outlook.com>
2024-02-25 22:27:44 +0100

    Adding migration to store user information

---
+++ TB.DanceDance.Data.PostgreSQL/Migrations/20240225212715_UserDisplayNameForAssignRequest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TB.DanceDance.Data.PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserDisplayNameForAssignRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserDisplayName",
+                schema: "access",
+                table: "GroupAssigmentRequests",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserDisplayName",
+                schema: "access",
+                table: "EventAssigmentRequests",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserDisplayName",
+                schema: "access",
+                table: "GroupAssigmentRequests");
+
+            migrationBuilder.DropColumn(
+                name: "UserDisplayName",
+                schema: "access",
+                table: "EventAssigmentRequests");
+        }
+    }
+}
