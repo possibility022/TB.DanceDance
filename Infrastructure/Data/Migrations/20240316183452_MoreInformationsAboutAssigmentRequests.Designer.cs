e02707a4b8db5f6b069bae2ff4945a77b4fb6438
Possibility <tomaszbakk@outlook.com>
2024-03-16 19:36:21 +0100

    new migration for AssigmentRequests changes

--- Infrastructure/Data/Migrations/20240316183452_MoreInformationsAboutAssigmentRequests.Designer.cs
+++ Infrastructure/Data/Migrations/20240316183452_MoreInformationsAboutAssigmentRequests.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace TB.DanceDance.Data.PostgreSQL.Migrations
 {
     [DbContext(typeof(DanceDbContext))]
-    partial class DanceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240316183452_MoreInformationsAboutAssigmentRequests")]
+    partial class MoreInformationsAboutAssigmentRequests
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
