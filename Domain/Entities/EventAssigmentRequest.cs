98c01c893c1f982027cd21be8fcaa0ae4199823e
Possibility <tomaszbakk@outlook.com>
2024-03-16 19:37:21 +0100

    This is not required

--- Domain/Entities/EventAssigmentRequest.cs
+++ Domain/Entities/EventAssigmentRequest.cs
@@ -6,16 +6,4 @@ public class EventAssigmentRequest : AssigmentRequestBase
     
     public required Guid EventId { get; init; }
 
-    /// <summary>
-    /// When <code>true</code>, then approved.
-    /// When <code>false</code> then rejected.
-    /// When <code>null</code> then no action taken.
-    /// </summary>
-    public bool? Approved { get; set; } = false;
-
-    /// <summary>
-    /// A user who accepted or rejected request.
-    /// </summary>
-    public string? ManagedBy { get; set; } = null;
-
 }
