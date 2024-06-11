92c51d8cf5eb6c7703fab79450d5089387965aef
Possibility <tomaszbakk@outlook.com>
2024-06-12 00:20:37 +0200

    Working on loading configuration - using environments variables

--- TB.DanceDance.Services.Converter.Deamon/Program.cs
+++ TB.DanceDance.Services.Converter.Deamon/Program.cs
@@ -5,16 +5,16 @@ ProgramConfig.Configure();
 
 using var oauthClient = new HttpClient()
 {
-    BaseAddress = new Uri(ProgramConfig.Settings.OAuthOrigin)
+    BaseAddress = new Uri(ProgramConfig.Instance.OAuthOrigin)
 };
 
-var tokenProvider = new TokenProvider(oauthClient, ProgramConfig.TokenProviderOptions);
+var tokenProvider = new TokenProvider(oauthClient, ProgramConfig.Instance.TokenProviderOptions);
 
 var handler = new TokenHttpHandler(tokenProvider);
 
 using var apiHttpClient = new HttpClient(handler)
 {
-    BaseAddress = new Uri(ProgramConfig.Settings.ApiOrigin)
+    BaseAddress = new Uri(ProgramConfig.Instance.ApiOrigin)
 };
 
 using var defaultHttpClient = new HttpClient();
--- TB.DanceDance.Services.Converter.Deamon/ProgramConfig.cs
+++ TB.DanceDance.Services.Converter.Deamon/ProgramConfig.cs
@@ -5,47 +5,95 @@ using TB.DanceDance.Services.Converter.Deamon.OAuthClient;
 namespace TB.DanceDance.Services.Converter.Deamon;
 internal class ProgramConfig
 {
-    public static class Settings
+    public static ProgramConfig Instance { get; private set; } = new ProgramConfig();
+
+    public string FFMPGDefaultFolder { get; internal set; } = "D:\\Programy\\ffmpeg-2022-12-04-git-6c814093d8-full_build\\bin";
+
+    public string ApiOrigin { get; internal set; } = string.Empty;
+    public string OAuthOrigin { get; internal set; } = string.Empty;
+
+    public TokenProviderOptions TokenProviderOptions { get; private set; } = null!;
+
+    private static bool TryGetEnvironmentVariable(string key, out string value)
     {
-        public const string FFMPGDefaultFolder = "D:\\Programy\\ffmpeg-2022-12-04-git-6c814093d8-full_build\\bin";
-
-        public static string ApiOrigin { get; internal set; } = string.Empty;
-        public static string OAuthOrigin { get; internal set; } = string.Empty;
+        value = string.Empty;
+        var v = Environment.GetEnvironmentVariable($"tb.dancedance.converter.{key}");
+        return !string.IsNullOrEmpty(v);
     }
-
-    public static TokenProviderOptions TokenProviderOptions { get; private set; } = null!;
-
 
     public static void Configure()
     {
+        var config = new ProgramConfig();
+
         ConfigureLogging();
         ConfigureFfmpeg();
-        ConfigureApi();
-        ConfigureAuth();
+        ConfigureApi(config);
+        ConfigureAuth(config);
+
+        ProgramConfig.Instance = config;
     }
 
-    private static void ConfigureAuth()
+    private static void ConfigureAuth(ProgramConfig config)
     {
-        var lines = File.ReadAllLines("auth.set.txt");
-        TokenProviderOptions = new TokenProviderOptions()
+        var scopeSet = TryGetEnvironmentVariable("scope", out var scope);
+        var clientIdSet = TryGetEnvironmentVariable("clientId", out var clientId);
+        var clientSecretSet = TryGetEnvironmentVariable("clientSecret", out var clientSecret);
+        var oAuthOriginSet = TryGetEnvironmentVariable("oAuthOrigin", out var oAuthOrigin);
+
+        if (scopeSet)
+            config.TokenProviderOptions.Scope = scope;
+
+        if (clientIdSet)
+            config.TokenProviderOptions.ClientId = clientId;
+
+        if (clientSecretSet)
+            config.TokenProviderOptions.ClientSecret = clientSecret;
+
+        if (oAuthOriginSet)
+            config.OAuthOrigin = oAuthOrigin;
+
+        if (File.Exists("auth.set.txt"))
         {
-            ClientId = lines[0].Trim(),
-            ClientSecret = lines[1].Trim(),
-            Scope = lines[2].Trim()
-        };
+            var lines = File.ReadAllLines("auth.set.txt");
+            config.TokenProviderOptions = new TokenProviderOptions()
+            {
+                ClientId = lines[0].Trim(),
+                ClientSecret = lines[1].Trim(),
+                Scope = lines[2].Trim()
+            };
+            config.OAuthOrigin = lines[3];
 
-        Settings.OAuthOrigin = lines[3];
+        }
+        if (string.IsNullOrEmpty(config.TokenProviderOptions.Scope))
+            Log.Warning("Scope is not set");
+
+        if (string.IsNullOrEmpty(config.TokenProviderOptions.ClientId))
+            Log.Warning("ClientId is not set");
+
+        if (string.IsNullOrEmpty(config.TokenProviderOptions.ClientSecret))
+            Log.Warning("ClientSecret is not set");
+
+        if (string.IsNullOrEmpty(config.OAuthOrigin))
+            Log.Warning("OAuthOrigin is not set");
     }
 
-    private static void ConfigureApi()
+    private static void ConfigureApi(ProgramConfig config)
     {
+        var apiSet = TryGetEnvironmentVariable("apiOrigin", out var api);
+        if (apiSet)
+            config.ApiOrigin = api;
+
         var lines = File.ReadAllLines("api.set.txt");
-        Settings.ApiOrigin = lines[0].Trim();
+        if (!string.IsNullOrEmpty(lines[0]?.Trim()))
+            config.ApiOrigin = lines[0].Trim();
+
+        if (string.IsNullOrEmpty(config.ApiOrigin))
+            Log.Warning("Api Origin is not set");
     }
 
     private static void ConfigureFfmpeg()
     {
-        string path = Settings.FFMPGDefaultFolder;
+        string path = Instance.FFMPGDefaultFolder;
 
         if (File.Exists("ffmpgpath.txt"))
         {
