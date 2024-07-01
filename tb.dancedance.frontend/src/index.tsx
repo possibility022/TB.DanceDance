7f208fce9273aa6f59297082a8ba0bdfedc98d4f
Possibility <tomaszbakk@outlook.com>
2024-07-01 21:17:46 +0200

    Missing changes for react migrations

--- tb.dancedance.frontend/src/index.tsx
+++ tb.dancedance.frontend/src/index.tsx
@@ -1,14 +1,17 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import { Container, createRoot } from 'react-dom/client';
 import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 
-ReactDOM.render(
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+const root = createRoot(document.getElementById("root") as Container)
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+root.render(
 	<React.StrictMode>
 		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
+	</React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
