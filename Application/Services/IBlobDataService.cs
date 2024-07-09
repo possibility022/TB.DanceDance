25360a395b19a555f48db7ef50e00707354f624c
Possibility <tomaszbakk@outlook.com>
2024-07-09 21:42:35 +0200

    handling token - commiting missing interface

--- Application/Services/IBlobDataService.cs
+++ Application/Services/IBlobDataService.cs
@@ -5,8 +5,8 @@ namespace Application.Services;
 public interface IBlobDataService
 {
     Uri GetSas(string blobId);
-    Task<Stream> OpenStream(string blobName);
-    Task Upload(string blobId, Stream stream);
+    Task<Stream> OpenStream(string blobName, CancellationToken token);
+    Task Upload(string blobId, Stream stream, CancellationToken token);
     SharedBlob CreateUploadSas(string blobId = null);
-    Task<bool> BlobExistsAsync(string blobId);
+    Task<bool> BlobExistsAsync(string blobId, CancellationToken token);
 }
