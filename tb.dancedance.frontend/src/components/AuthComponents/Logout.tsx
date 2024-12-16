b754c3510e0043c6786ba6c89152e1b227bd0707
Possibility <tomaszbakk@outlook.com>
2024-12-16 01:46:36 +0100

    Ignoring one lint issue

--- tb.dancedance.frontend/src/components/AuthComponents/Logout.tsx
+++ tb.dancedance.frontend/src/components/AuthComponents/Logout.tsx
@@ -4,6 +4,8 @@ import { IAuthService } from "../../services/AuthService";
 
 export const Logout = () => (
     <AuthConsumer>
+        {/*todo - fix linted issue */}
+        {/* eslint-disable-next-line @typescript-eslint/unbound-method */}
         {({ logout }: IAuthService) => {
             logout().catch(e => console.error(e));
             return <span>loading</span>;
