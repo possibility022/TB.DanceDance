cddbd5d070c1df88650c7bc27bc9ceb31e9f1626
Possibility <tomaszbakk@outlook.com>
2024-12-17 03:00:40 +0100

    delete old script

--- startDb.ps1
+++
@@ -1,7 +0,0 @@
-$mongoDbExe = 'D:\Programy\mongodb-win32-x86_64-windows-6.0.4\bin\mongod.exe'
-$mongoArgs = @('--dbpath="D:\data\dancedance\mongodb"')
-
-#Start-Process $mongoDbExe -ArgumentList $mongoArgs
-
-# npm install -g azurite
-azurite -l d:\data\dancedance\azurite\
