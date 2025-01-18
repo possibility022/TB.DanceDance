1564477133663b3bff073f46903f3b5ece50dba6
Possibility <tomaszbakk@outlook.com>
2025-01-18 19:53:49 +0100

    WIP - I can load login website

---
+++ src/TB.DanceDance.Mobile/MauiAuthenticationBrowser.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Duende Software. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using IdentityModel.Client;
+using IdentityModel.OidcClient.Browser;
+
+namespace TB.DanceDance.Mobile;
+
+public class MauiAuthenticationBrowser : IdentityModel.OidcClient.Browser.IBrowser
+{
+    public async Task<BrowserResult> InvokeAsync(BrowserOptions options, CancellationToken cancellationToken = new CancellationToken())
+    {
+        try
+        {
+            var result = await WebAuthenticator.Default.AuthenticateAsync(
+                new Uri(options.StartUrl),
+                new Uri(options.EndUrl));
+
+            var url = new RequestUrl("myapp://")
+                .Create(new Parameters(result.Properties));
+
+            return new BrowserResult
+            {
+                Response = url,
+                ResultType = BrowserResultType.Success,
+            };
+        }
+        catch (TaskCanceledException)
+        {
+            return new BrowserResult
+            {
+                ResultType = BrowserResultType.UserCancel
+            };
+        }
+    }
+}
--- src/TB.DanceDance.Mobile/Pages/MainPage.xaml.cs
+++ src/TB.DanceDance.Mobile/Pages/MainPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using TB.DanceDance.Mobile.Models;
-using TB.DanceDance.Mobile.PageModels;
+﻿using IdentityModel;
+using IdentityModel.Client;
+using IdentityModel.OidcClient;
+using System.Diagnostics;
+using System.Text;
 
 namespace TB.DanceDance.Mobile.Pages;
 
@@ -9,5 +12,60 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
         BindingContext = model;
+    }
+
+    private async void Button_OnClicked(object? sender, EventArgs e)
+    {
+        try
+        {
+            var client = new OidcClient(new OidcClientOptions()
+            {
+                //Authority = "https://localhost:7068",
+                Authority = "http://10.0.2.2:5068",
+                ClientId = "tbdancedancefront",
+                RedirectUri = "myapp://",
+                Scope = "openid tbdancedanceapi.read offline_access profile",
+                Browser = new MauiAuthenticationBrowser(),
+                Policy = new Policy()
+                {
+                    Discovery = new DiscoveryPolicy()
+                    {
+                        RequireHttps = false
+                    }
+                }
+                
+            });
+
+            var result = await client.LoginAsync();
+
+            var currentAccessToken = result.AccessToken;
+
+            Debug.WriteLine(currentAccessToken);
+
+            var sb = new StringBuilder(128);
+
+            sb.AppendLine("claims:");
+            foreach (var claim in result.User.Claims)
+            {
+                sb.AppendLine($"{claim.Type}: {claim.Value}");
+            }
+
+            sb.AppendLine();
+            sb.AppendLine("access token:");
+            sb.AppendLine(result.AccessToken);
+
+            if (!string.IsNullOrWhiteSpace(result.RefreshToken))
+            {
+                sb.AppendLine();
+                sb.AppendLine("refresh token:");
+                sb.AppendLine(result.RefreshToken);
+            }
+
+            Debug.Write(sb.ToString());
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.ToString());    
+        }
     }
 }
