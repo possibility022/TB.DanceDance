5904e82d82ff52e5d6d01dfc6e9fd6662b87e8f3
Possibility <tomaszbakk@outlook.com>
2025-03-01 21:10:49 +0100

    Refactor BlobUploader to support stream uploads and add MemoryStreamWrapper for cancellation simulation in tests

--- src/TB.DanceDance.Mobile.Tests/IntegrationTests/BlobUploaderTests.cs
+++ src/TB.DanceDance.Mobile.Tests/IntegrationTests/BlobUploaderTests.cs
@@ -18,17 +18,7 @@ public class BlobUploaderTests : IAsyncLifetime
     public async Task UploadWorks_ContentIsCorrect()
     {
         var blob = client.GetBlobClient(Guid.NewGuid().ToString());
-        var sasBuilder = new BlobSasBuilder
-        {
-            BlobContainerName = client.Name,
-            BlobName = blob.Name,
-            Resource = "b",
-            ExpiresOn = DateTimeOffset.UtcNow.AddHours(1)
-        };
-        sasBuilder.SetPermissions(BlobSasPermissions.Create | BlobSasPermissions.Add | BlobSasPermissions.Read |
-                                  BlobSasPermissions.Write);
-
-        var uri = blob.GenerateSasUri(sasBuilder);
+        Uri uri = GenerateSas(blob);
 
         using MemoryStream ms = new();
         for (int i = 0; i < 350; i++)
@@ -42,6 +32,53 @@ public class BlobUploaderTests : IAsyncLifetime
 
         ms.Position = 0;
 
+        // Verify the blob was uploaded
+        var download = await blob.DownloadAsync();
+        using MemoryStream downloadedMs = new();
+        await download.Value.Content.CopyToAsync(downloadedMs);
+
+        // Check if the content is correct
+        Assert.Equal(ms.ToArray(), downloadedMs.ToArray());
+    }
+
+    private Uri GenerateSas(BlobClient blob)
+    {
+        var sasBuilder = new BlobSasBuilder
+        {
+            BlobContainerName = client.Name,
+            BlobName = blob.Name,
+            Resource = "b",
+            ExpiresOn = DateTimeOffset.UtcNow.AddHours(1)
+        };
+        sasBuilder.SetPermissions(BlobSasPermissions.Create | BlobSasPermissions.Add | BlobSasPermissions.Read |
+                                  BlobSasPermissions.Write);
+
+        var uri = blob.GenerateSasUri(sasBuilder);
+        return uri;
+    }
+
+    [Fact]
+    public async Task Upload_CanContinueLater()
+    {
+        var blob = client.GetBlobClient(Guid.NewGuid().ToString());
+        var uri = GenerateSas(blob);
+        var cancellationTokenSource = new CancellationTokenSource();
+        
+        using MemoryStream ms = new();
+        using MemoryStreamWrapper msWrapper = new(ms, 125, cancellationTokenSource);
+        for (int i = 0; i < 350; i++)
+        {
+            ms.WriteByte(1);
+        }
+
+        ms.Position = 0;
+
+        await Assert.ThrowsAsync<TaskCanceledException>(async () =>  await blobUploader.UploadFileAsync(msWrapper, uri, cancellationTokenSource.Token));
+            
+        ms.Position = 0;
+        
+        await blobUploader.ResumeUploadAsync(ms, uri, CancellationToken.None);
+        
         // Verify the blob was uploaded
         var download = await blob.DownloadAsync();
         using MemoryStream downloadedMs = new();
--- src/TB.DanceDance.Mobile/Services/DanceApi/BlobUploader.cs
+++ src/TB.DanceDance.Mobile/Services/DanceApi/BlobUploader.cs
@@ -1,5 +1,4 @@
-﻿using Azure.Storage.Blobs;
-using Azure.Storage.Blobs.Models;
+﻿using Azure.Storage.Blobs.Models;
 using Azure.Storage.Blobs.Specialized;
 
 namespace TB.DanceDance.Mobile.Services.DanceApi
@@ -17,7 +16,7 @@ namespace TB.DanceDance.Mobile.Services.DanceApi
             byte[] buffer = new byte[BufferSize];
             int bytesRead;
             int blockId = 0;
-            
+
             while ((bytesRead = await stream.ReadAsync(buffer, 0, BufferSize, cancellationToken)) > 0)
             {
                 var blockIdBase64 = Convert.ToBase64String(BitConverter.GetBytes(blockId));
@@ -33,7 +32,7 @@ namespace TB.DanceDance.Mobile.Services.DanceApi
             await blobClient.CommitBlockListAsync(blockList, cancellationToken: cancellationToken);
         }
 
-        public async Task ResumeUploadAsync(string filePath, Uri blobUri, CancellationToken cancellationToken)
+        public async Task ResumeUploadAsync(Stream stream, Uri blobUri, CancellationToken cancellationToken)
         {
             var blobClient = new BlockBlobClient(blobUri);
 
@@ -44,22 +43,19 @@ namespace TB.DanceDance.Mobile.Services.DanceApi
             byte[] buffer = new byte[BufferSize];
             int blockId = blockList.Count;
 
-            await using (var fileStream = File.OpenRead(filePath))
+            stream.Seek(blockId * BufferSize, SeekOrigin.Begin);
+
+            int bytesRead;
+            while ((bytesRead = await stream.ReadAsync(buffer, 0, BufferSize, cancellationToken)) > 0)
             {
-                fileStream.Seek(blockId * BufferSize, SeekOrigin.Begin);
-
-                int bytesRead;
-                while ((bytesRead = await fileStream.ReadAsync(buffer, 0, BufferSize, cancellationToken)) > 0)
+                var blockIdBase64 = Convert.ToBase64String(BitConverter.GetBytes(blockId));
+                using (var memoryStream = new MemoryStream(buffer, 0, bytesRead))
                 {
-                    var blockIdBase64 = Convert.ToBase64String(BitConverter.GetBytes(blockId));
-                    using (var memoryStream = new MemoryStream(buffer, 0, bytesRead))
-                    {
-                        await blobClient.StageBlockAsync(blockIdBase64, memoryStream, null, cancellationToken);
-                    }
-
-                    blockList.Add(blockIdBase64);
-                    blockId++;
+                    await blobClient.StageBlockAsync(blockIdBase64, memoryStream, null, cancellationToken);
                 }
+
+                blockList.Add(blockIdBase64);
+                blockId++;
             }
 
             await blobClient.CommitBlockListAsync(blockList, cancellationToken: cancellationToken);
