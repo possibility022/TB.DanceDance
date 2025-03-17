fdaa5b2f9ad9b68ea662339adf2d2d2a56f3f688
Possibility <tomaszbakk@outlook.com>
2025-03-17 01:18:12 +0100

    work in progress - working binding

---
+++ src/TB.DanceDance.Mobile/Models/Event.cs
@@ -0,0 +1,17 @@
+﻿using TB.DanceDance.API.Contracts.Responses;
+
+namespace TB.DanceDance.Mobile.Models;
+
+public record Event
+{
+    public string Name { get; set; }
+    public DateTime When { get; set; }
+    public Guid Id { get; set; }
+
+    public static List<Event> MapFromApiEvent(UserEventsAndGroupsResponse response)
+    {
+        return response.Assigned.Events
+            .Select(r => new Event() { Name = r.Name, When = r.Date, Id = r.Id })
+            .ToList();
+    }
+}
---
+++ src/TB.DanceDance.Mobile/PageModels/EventsPageModel.cs
@@ -0,0 +1,69 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Diagnostics;
+using TB.DanceDance.Mobile.Models;
+using TB.DanceDance.Mobile.Services.DanceApi;
+
+namespace TB.DanceDance.Mobile.PageModels;
+
+public partial class EventsPageModel : ObservableObject
+{
+    private readonly DanceHttpApiClient _apiClient;
+
+    public EventsPageModel(DanceHttpApiClient apiClient)
+    {
+        _apiClient = apiClient;
+    }
+    
+    [ObservableProperty] bool _isRefreshing;
+    
+    [ObservableProperty] private List<Event> _userEvents = [new Event()
+    {
+        Id = Guid.Empty
+        ,Name = "test",
+        When = DateTime.Now
+    }];
+    private bool _isInitialized;
+
+    [RelayCommand]
+    private async Task Appearing()
+    {
+        Debug.WriteLine("Appearing Binded"); 
+    }
+
+    [RelayCommand]
+    private async Task AddEvent()
+    {
+        Debug.WriteLine("AddEvent");
+        UserEvents = new List<Event>()
+        {
+            new Event() { Id = Guid.Empty, Name = "test", When = DateTime.Now },
+            new Event() { Id = Guid.Empty, Name = "test2", When = DateTime.Now },
+        };
+    }
+    
+    [RelayCommand]
+    private async Task Refresh()
+    {
+        try
+        {
+            IsRefreshing = true;
+            await LoadData();
+        }
+        catch (Exception e)
+        {
+            //_errorHandler.HandleError(e);
+        }
+        finally
+        {
+            IsRefreshing = false;
+        }
+    }
+
+    private async Task LoadData()
+    {
+        var acesses = await _apiClient.GetUserAccesses();
+        if (acesses != null)
+            UserEvents = Event.MapFromApiEvent(acesses);
+    }
+}
