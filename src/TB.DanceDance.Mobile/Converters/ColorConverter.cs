86e64f051dee830494beef5860f63560aac636c7
Possibility <tomaszbakk@outlook.com>
2025-05-18 16:42:36 +0200

    Add ColorConverter for dynamic background color binding in UploadManagerPage

---
+++ src/TB.DanceDance.Mobile/Converters/ColorConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace TB.DanceDance.Mobile.Converters;
+
+public class ColorConverter : IValueConverter
+{
+    object IValueConverter.Convert(object? value, Type targetType, object? parameter, CultureInfo info)
+    {
+        if ((bool)value!)
+            return Color.FromArgb(parameter!.ToString()); 
+        else
+            return Colors.Transparent;
+    }
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
