7c6b5a67619f520542f371c60cea92975413843d
Possibility <tomaszbakk@outlook.com>
2025-07-28 17:33:53 +0200

    feat: add rename functionality for videos in group

--- src/TB.DanceDance.Mobile/PageModels/GroupVideosPageModel.cs
+++ src/TB.DanceDance.Mobile/PageModels/GroupVideosPageModel.cs
@@ -1,17 +1,21 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Serilog;
 using TB.DanceDance.Mobile.Data;
 using TB.DanceDance.Mobile.Data.Models;
+using TB.DanceDance.Mobile.Services.DanceApi;
 
 namespace TB.DanceDance.Mobile.PageModels;
 
 public partial class GroupVideosPageModel : ObservableObject
 {
     private readonly VideoProvider videoProvider;
+    private readonly DanceHttpApiClient apiClient;
 
-    public GroupVideosPageModel(VideoProvider videoProvider)
+    public GroupVideosPageModel(VideoProvider videoProvider, DanceHttpApiClient apiClient)
     {
         this.videoProvider = videoProvider;
+        this.apiClient = apiClient;
     }
     
     [ObservableProperty] private bool isRefreshing;
@@ -42,6 +46,42 @@ public partial class GroupVideosPageModel : ObservableObject
         finally
         {
             IsRefreshing = false;
+        }
+    }
+    
+    
+    [RelayCommand]
+    private async Task RenameVideo(Guid videoId)
+    {
+        // TODO this code is duplicated in event details page model.
+        // Refactor it in future.
+        try
+        {
+            var newName = await Shell.Current.CurrentPage.DisplayPromptAsync("Zmień nazwę",
+                "Podaj nową nazwę dla nagrania.", "Ok",
+                "Anuluj");
+            
+            if (newName == null)
+                return;
+            
+            var video = Videos.First(r => r.Id == videoId);
+            if (video.Name == newName)
+                return;
+
+            if (newName.Length is < 5 or > 50)
+            {
+                await Shell.Current.CurrentPage.DisplayAlert("Zła nazwa", "Nazwa musi mieć od 5 do 50 znaków.", "Ok");
+                return;
+            }
+            
+            await apiClient.RenameVideoAsync(videoId, newName);
+            video.Name = newName;
+
+            await Refresh();
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "Could not rename video");
         }
     }
     
