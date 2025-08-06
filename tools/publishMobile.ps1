2c3607be7bf61cfefd3e74e88a4c36b0bab90b32
Possibility <tomaszbakk@outlook.com>
2025-08-07 01:19:37 +0200

    publishScript is now also editing version

--- tools/publishMobile.ps1
+++ tools/publishMobile.ps1
@@ -18,6 +18,28 @@ $password = Read-Host "Podaj hasło do keystore" -AsSecureString
 $originalLocation = Get-Location
 $mobileDir = Join-Path $scriptDir '..\src\TB.DanceDance.Mobile'
 
+$increaseVersion = Read-Host -Prompt "Chcesz podnieść wersję? y/N"
+
+if ($increaseVersion -eq "Y" -or $increaseVersion -eq "y"){
+    $mobileSolution = Join-Path $mobileDir 'TB.DanceDance.Mobile.csproj'
+    $csProjContent = Get-Content $mobileSolution -Raw
+    # Extract all version numbers between <ApplicationVersion> tags
+    $versions = [regex]::Matches($csProjContent, "<ApplicationVersion>(\d+)</ApplicationVersion>") | ForEach-Object {
+        [int]$_.Groups[1].Value
+    }
+
+    # Find the highest version
+    $maxVersion = ($versions | Measure-Object -Maximum).Maximum
+    $toReplace = "<ApplicationVersion>" + $maxVersion + "</ApplicationVersion>"
+    $newVersion = [int]$maxVersion + 1;
+    $toSet = "<ApplicationVersion>" + $newVersion + "</ApplicationVersion>"
+    $csProjContent = $csProjContent.Replace($toReplace, $toSet)
+
+    Write-Output "Zmiana wersji z: " + $toReplace + "na: " $toSet
+
+    Out-File $mobileSolution -Encoding utf8 -InputObject $csProjContent -NoNewline
+}
+
 $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
 $UnsecurePassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
 Set-Content -Path $passwordPath -Value $UnsecurePassword -NoNewline
