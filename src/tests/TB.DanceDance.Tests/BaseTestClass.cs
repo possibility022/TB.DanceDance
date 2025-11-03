f1ce97279c6850e5a28d96fc2121048daa735398
Possibility <tomaszbakk@outlook.com>
2025-11-03 01:18:35 +0100

    base class for tests

---
+++ src/tests/TB.DanceDance.Tests/BaseTestClass.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure.Data;
+
+namespace TB.DanceDance.Tests;
+
+public abstract class BaseTestClass : IAsyncLifetime
+{
+    protected readonly DanceDbContext SeedDbContext;
+    private readonly DanceDbContext runtimeDbContext;
+    
+    protected BaseTestClass(DanceDbFixture dbContextFixture)
+    {
+        SeedDbContext = dbContextFixture.DbContextFactory();
+        runtimeDbContext = dbContextFixture.DbContextFactory();
+    }
+
+    protected abstract ValueTask Initialize(DanceDbContext runtimeDbContext);
+
+    public ValueTask DisposeAsync()
+    {
+        return SeedDbContext.DisposeAsync();
+    }
+
+    public ValueTask InitializeAsync()
+    {
+        return Initialize(runtimeDbContext);
+    }
+}
