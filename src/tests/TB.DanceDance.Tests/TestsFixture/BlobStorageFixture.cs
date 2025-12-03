cfee899f4855ae02985052846027e65cbb3b0652
Possibility <tomaszbakk@outlook.com>
2025-12-03 23:53:15 +0100

    lets try with azurite manually installed and started

--- src/tests/TB.DanceDance.Tests/TestsFixture/BlobStorageFixture.cs
+++ src/tests/TB.DanceDance.Tests/TestsFixture/BlobStorageFixture.cs
@@ -5,27 +5,47 @@ using Testcontainers.Azurite;
 
 namespace TB.DanceDance.Tests.TestsFixture;
 
-public class BlobStorageFixture() : IAsyncLifetime
+public class BlobStorageFixture : IAsyncLifetime
 {
     private const string AzuriteImage = "mcr.microsoft.com/azure-storage/azurite";
+
+    private const string AzuriteManuallyHostedConnectionString = 
+        "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
+
+    private readonly AzuriteContainer? container;
+
+    public BlobStorageFixture()
+    {
+        if (!IsStartedManually())
+            container = new AzuriteBuilder()
+                .WithImage(AzuriteImage)
+                .Build();
+    }
     
-    private readonly AzuriteContainer container = new AzuriteBuilder()
-        .WithImage(AzuriteImage)
-        .Build();
+    private bool IsStartedManually() => 
+        "true".Equals(Environment.GetEnvironmentVariable("ManualAzuriteConfigured"),
+            StringComparison.CurrentCultureIgnoreCase);
 
 
     public string GetConnectionString()
     {
-        return container.GetConnectionString();
+        if (container is not null)
+            return container.GetConnectionString();
+
+        return AzuriteManuallyHostedConnectionString;
     }
 
     public ValueTask DisposeAsync()
     {
-        return container.DisposeAsync();
+        if (container is not null)
+            return container.DisposeAsync();
+
+        return ValueTask.CompletedTask;
     }
 
     public async ValueTask InitializeAsync()
     {
-        await container.StartAsync();
+        if (container is not null)
+            await container.StartAsync();
     }
 }
