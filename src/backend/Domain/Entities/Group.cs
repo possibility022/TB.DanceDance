754b53b6c73dbaaa6cc3457e2a61246bdf4085e0
Possibility <tomaszbakk@outlook.com>
2025-12-20 15:33:45 +0100

    adding new migration - support seasons

--- src/backend/Domain/Entities/Group.cs
+++ src/backend/Domain/Entities/Group.cs
@@ -4,6 +4,9 @@ public class Group
 {
     public Guid Id { get; set; }
     public required string Name { get; set; }
+    
+    public required DateOnly SeasonStart { get; set; }
+    public required DateOnly SeasonEnd { get; set; }
 
     public ICollection<SharedWith> HasSharedVideos { get; set; } = null!;
 }
