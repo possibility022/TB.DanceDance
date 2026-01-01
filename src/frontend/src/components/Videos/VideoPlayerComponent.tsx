9305590e975765bc5eadc050c7990e5b671768d4
Possibility <tomaszbakk@outlook.com>
2026-01-01 23:54:07 +0100

    extract video player to its own component

---
+++ src/frontend/src/components/Videos/VideoPlayerComponent.tsx
@@ -0,0 +1,35 @@
+﻿import React from 'react';
+import ReactPlayer from "react-player";
+import {VideoList} from "./VideoList";
+import {BlobId} from "../../types/ApiModels/TypeIds";
+import VideoInformation from "../../types/ApiModels/VideoInformation";
+import {SharedScope} from "../../types/appTypes";
+
+export interface VideoPlayerComponentProps {
+    videoId: BlobId | undefined;
+    sharedScope: SharedScope | undefined;
+    videoList: VideoInformation[];
+    url: string | undefined;
+}
+
+function VideoPlayerComponent(props: VideoPlayerComponentProps) {
+    return (
+        <div>
+            <ReactPlayer
+                width='100%'
+                height='100%'
+                controls={true}
+                config={{ file: { attributes: { controlsList: 'nodownload' } } }}
+                onContextMenu={(e: Event) => e.preventDefault()}
+
+                url={props.url} />
+
+            <VideoList videos={props.videoList}
+                       sharedScope={props.sharedScope}
+                       enableShare={false}
+                       selectedVideo={props.videoId}></VideoList>
+        </div>
+    );
+}
+
+export default VideoPlayerComponent;
