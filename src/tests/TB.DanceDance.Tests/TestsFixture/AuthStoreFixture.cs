6f718e2df8fd35a21bbb4b596f054e11cb41db11
Possibility <tomaszbakk@outlook.com>
2026-04-30 23:27:46 +0200

    add tests for auth store migrations

---
+++ src/tests/TB.DanceDance.Tests/TestsFixture/AuthStoreFixture.cs
@@ -0,0 +1,28 @@
+﻿using TB.Auth.Web.Identity;
+using Testcontainers.PostgreSql;
+
+namespace TB.DanceDance.Tests.TestsFixture;
+
+public class AuthStoreFixture : IAsyncLifetime
+{
+    private const string PostgresImage = "postgres";
+
+    private readonly PostgreSqlContainer container = new PostgreSqlBuilder(PostgresImage)
+        .Build();
+
+    
+    public AuthStoreContext DbContextFactory()
+    {
+        return DesignTimeContextFactory.CreateAuthStoreContext(container.GetConnectionString());
+    }
+    
+    public ValueTask DisposeAsync()
+    {
+        return container.DisposeAsync();
+    }
+
+    public async ValueTask InitializeAsync()
+    {
+        await container.StartAsync();
+    }
+}
