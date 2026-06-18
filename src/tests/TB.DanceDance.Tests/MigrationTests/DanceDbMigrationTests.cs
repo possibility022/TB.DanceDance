653f3c8a148f0dc1026e3d20506f5eb3a9f5c181
Possibility <tomaszbakk@outlook.com>
2026-06-19 00:21:24 +0200

    Refactor `DanceDbMigrationTests` to use per-class connection strings
    - Introduced `connectionString` for class-specific context in migration tests.
    - Updated `DbContextFactory` calls to use the new `connectionString`.
    - Initialized `connectionString` via `DanceDbFixture` in the constructor.

--- src/tests/TB.DanceDance.Tests/MigrationTests/DanceDbMigrationTests.cs
+++ src/tests/TB.DanceDance.Tests/MigrationTests/DanceDbMigrationTests.cs
@@ -6,7 +6,13 @@ namespace TB.DanceDance.Tests.MigrationTests;
 [TestCaseOrderer(typeof(PriorityOrderer))]
 public class DanceDbMigrationTests : IAsyncLifetime
 {
-    private readonly DanceDbFixture dbFixture = new DanceDbFixture();
+    private readonly DanceDbFixture dbFixture = new();
+    private readonly string connectionString;
+
+    public DanceDbMigrationTests()
+    {
+        connectionString = dbFixture.GetConnectionStringForThisClassSet(TestContext.Current);
+    }
     
     public async ValueTask DisposeAsync()
     {
@@ -21,12 +27,12 @@ public class DanceDbMigrationTests : IAsyncLifetime
     [Fact, TestPriority(2)]
     public async Task UpMigrationsCanRun()
     {
-        await dbFixture.DbContextFactory(dbFixture.DefaultConnectionString).Database.MigrateAsync(TestContext.Current.CancellationToken);
+        await dbFixture.DbContextFactory(connectionString).Database.MigrateAsync(TestContext.Current.CancellationToken);
     }
     
     [Fact, TestPriority(1)]
     public async Task DownMigrationsCanRun()
     {
-        await dbFixture.DbContextFactory(dbFixture.DefaultConnectionString).Database.MigrateAsync("20230617222723_Initial", TestContext.Current.CancellationToken);
+        await dbFixture.DbContextFactory(connectionString).Database.MigrateAsync("20230617222723_Initial", TestContext.Current.CancellationToken);
     }
 }
