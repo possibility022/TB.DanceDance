9f6c7e2a932c89ae64b4d976f44170fc8c439485
Possibility <tomaszbakk@outlook.com>
2026-06-19 00:30:46 +0200

    Refactor `DanceDbMigrationTests` to initialize `connectionString` in `InitializeAsync`
    - Moved `connectionString` initialization from the constructor to `InitializeAsync`.
    - Adjusted test priorities for `UpMigrationsCanRun` and `DownMigrationsCanRun`.

--- src/tests/TB.DanceDance.Tests/MigrationTests/DanceDbMigrationTests.cs
+++ src/tests/TB.DanceDance.Tests/MigrationTests/DanceDbMigrationTests.cs
@@ -7,12 +7,7 @@ namespace TB.DanceDance.Tests.MigrationTests;
 public class DanceDbMigrationTests : IAsyncLifetime
 {
     private readonly DanceDbFixture dbFixture = new();
-    private readonly string connectionString;
-
-    public DanceDbMigrationTests()
-    {
-        connectionString = dbFixture.GetConnectionStringForThisClassSet(TestContext.Current);
-    }
+    private string connectionString = string.Empty;
     
     public async ValueTask DisposeAsync()
     {
@@ -22,15 +17,16 @@ public class DanceDbMigrationTests : IAsyncLifetime
     public async ValueTask InitializeAsync()
     {
         await dbFixture.InitializeAsync();
+        connectionString = dbFixture.GetConnectionStringForThisClassSet(TestContext.Current);
     }
 
-    [Fact, TestPriority(2)]
+    [Fact, TestPriority(1)]
     public async Task UpMigrationsCanRun()
     {
         await dbFixture.DbContextFactory(connectionString).Database.MigrateAsync(TestContext.Current.CancellationToken);
     }
     
-    [Fact, TestPriority(1)]
+    [Fact, TestPriority(2)]
     public async Task DownMigrationsCanRun()
     {
         await dbFixture.DbContextFactory(connectionString).Database.MigrateAsync("20230617222723_Initial", TestContext.Current.CancellationToken);
