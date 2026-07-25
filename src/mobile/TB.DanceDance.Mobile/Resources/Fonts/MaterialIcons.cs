6506357ef7b830131f1b405223f2f3bd278c6549
Possibility <tomaszbakk@outlook.com>
2026-07-26 01:23:32 +0200

    Refactor UI elements to use consistent styles and improve layout spacing across multiple pages

---
+++ src/mobile/TB.DanceDance.Mobile/Resources/Fonts/MaterialIcons.cs
@@ -0,0 +1,16 @@
+namespace TB.DanceDance.Mobile.Resources.Fonts;
+
+public static class MaterialIcons
+{
+    public const string Event = "\ue878";
+    public const string Group = "\ue7ef";
+    public const string VideoLibrary = "\ue04a";
+    public const string AccountCircle = "\ue853";
+    public const string Share = "\ue80d";
+    public const string Edit = "\ue3c9";
+    public const string Delete = "\ue872";
+    public const string MoreVertical = "\ue5d4";
+    public const string AddVideo = "\ue070";
+    public const string AddEvent = "\ue614";
+    public const string Play = "\ue037";
+}
